975fba346c8e20483db92abae9657c71b66842af
asadaker <ruslan.takhautdinov@gmail.com>
2026-05-20 00:14:16 +0700

    changed file names of .md files

--- src/test/java/com/example/bankcards/controller/UserCardControllerTests.java
+++ src/test/java/com/example/bankcards/controller/UserCardControllerTests.java
@@ -77,7 +77,7 @@ class UserCardControllerTest {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       http
-              .csrf(csrf -> csrf.disable())  // <-- Отключаем CSRF
+              .csrf(csrf -> csrf.disable())
               .authorizeHttpRequests(auth -> auth
                       .anyRequest().authenticated()
               );
